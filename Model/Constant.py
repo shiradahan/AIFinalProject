a65223b03e5a0d179ce37d981590fd8ebb8ce6ac
Noabenzikry <noa.benzikry@mail.huji.ac.il>
2024-09-03 12:08:18 +0300

    Delete Model/Constant.py

--- Model/Constant.py
+++
@@ -1,4 +0,0 @@
-class Constant:
-    DAYS_NUM = 5  # Number of days for the camp
-    MAX_SESSION_CAPACITY = 15  # Max capacity per session
-    SESSION_DURATION = 1  # Assuming each session is 1 hour long
